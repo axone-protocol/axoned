29594444e8c749f975e28316dddb0b1322bfebfa
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2022-04-12 17:08:53 +0200

    feat: extend debug cmd adding decode-blocks

--- cmd/okp4d/main.go
+++ cmd/okp4d/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/okp4/okp4d/app"
+	"github.com/spf13/cobra"
 	"github.com/tendermint/starport/starport/pkg/cosmoscmd"
 )
 
@@ -18,7 +20,30 @@ func main() {
 		app.New,
 		// this line is used by starport scaffolding # root/arguments
 	)
+
+	if err := Extend(rootCmd); err != nil {
+		os.Exit(1)
+	}
+
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
 		os.Exit(1)
 	}
+}
+
+func Extend(cmd *cobra.Command) error {
+	cmdGetter := func(name string) (*cobra.Command, error) {
+		return getSubCommand(cmd, name)
+	}
+
+	return ExtendDebugCmd(cmdGetter)
+}
+
+func getSubCommand(cmd *cobra.Command, name string) (*cobra.Command, error) {
+	for i, v := range cmd.Commands() {
+		if v.Name() == name {
+			return cmd.Commands()[i], nil
+		}
+	}
+
+	return nil, fmt.Errorf("cannot find '%s' command", name)
 }
