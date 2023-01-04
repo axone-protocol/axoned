1ec1a442333f3115eab39f27964befa118e7a32f
ccamel <camel.christophe@gmail.com>
2023-01-04 22:45:32 +0100

    test(logic): add block_height/1 tests

---
+++ x/logic/predicate/block.test.go
@@ -0,0 +1,54 @@
+package predicate
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/cosmos/cosmos-sdk/store"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ichiban/prolog/engine"
+	"github.com/okp4/okp4d/x/logic/testutil"
+	"github.com/smartystreets/goconvey/convey"
+	"github.com/tendermint/tendermint/libs/log"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmdb "github.com/tendermint/tm-db"
+)
+
+//nolint:dupl
+func TestBlock(t *testing.T) {
+	cases := []struct {
+		header      tmproto.Header
+		implication string
+		ok          bool
+	}{
+		{header: tmproto.Header{Height: 102}, implication: `block_height(102)`, ok: true},
+		{header: tmproto.Header{Height: 905}, implication: `block_height(102)`, ok: false},
+		{header: tmproto.Header{Height: 102}, implication: `block_height(X), X == 102`, ok: true},
+		{header: tmproto.Header{Height: 102}, implication: `block_height(X), X == 905`, ok: true},
+	}
+	for _, tc := range cases {
+		convey.Convey(fmt.Sprintf("Given the clause body: %s", tc.implication), t, func() {
+			convey.Convey("Given a context", func() {
+				db := tmdb.NewMemDB()
+				stateStore := store.NewCommitMultiStore(db)
+				ctx := sdk.NewContext(stateStore, tc.header, false, log.NewNopLogger())
+
+				convey.Convey("and a vm", func() {
+					vm := testutil.NewVMMust(ctx)
+					vm.Register1(engine.NewAtom("block_height"), BlockHeight(ctx))
+					testutil.CompileMust(vm, ctx, fmt.Sprintf("test :- %s.", tc.implication))
+
+					convey.Convey("When the predicate is called", func() {
+						ok, err := vm.Arrive(engine.NewAtom("test"), []engine.Term{}, engine.Success, nil).Force(ctx)
+
+						convey.Convey("Then the result should be true and there should be no error", func() {
+							convey.So(err, convey.ShouldBeNil)
+							convey.So(ok, convey.ShouldEqual, tc.ok)
+						})
+					})
+				})
+			})
+		})
+	}
+}
