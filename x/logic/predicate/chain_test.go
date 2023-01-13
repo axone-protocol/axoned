49bfba3f7903f1cdeb413b3beef868dce08d1ffe
ccamel <camel.christophe@gmail.com>
2023-01-13 23:47:51 +0100

    test(logic): use dot import alias for goconvey

--- x/logic/predicate/chain_test.go
+++ x/logic/predicate/chain_test.go
@@ -8,7 +8,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ichiban/prolog/engine"
 	"github.com/okp4/okp4d/x/logic/testutil"
-	"github.com/smartystreets/goconvey/convey"
+	. "github.com/smartystreets/goconvey/convey"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
@@ -26,23 +26,23 @@ func TestChainID(t *testing.T) {
 		{header: tmproto.Header{ChainID: "okp4-nemeton-1"}, implication: `chain_id(X), X == "akashnet-2"`, ok: false},
 	}
 	for _, tc := range cases {
-		convey.Convey(fmt.Sprintf("Given the clause body: %s", tc.implication), t, func() {
-			convey.Convey("Given a context", func() {
+		Convey(fmt.Sprintf("Given the clause body: %s", tc.implication), t, func() {
+			Convey("Given a context", func() {
 				db := tmdb.NewMemDB()
 				stateStore := store.NewCommitMultiStore(db)
 				ctx := sdk.NewContext(stateStore, tc.header, false, log.NewNopLogger())
 
-				convey.Convey("and a vm", func() {
+				Convey("and a vm", func() {
 					vm := testutil.NewVMMust(ctx)
 					vm.Register1(engine.NewAtom("chain_id"), ChainID)
 					testutil.CompileMust(ctx, vm, fmt.Sprintf("test :- %s.", tc.implication))
 
-					convey.Convey("When the predicate is called", func() {
+					Convey("When the predicate is called", func() {
 						ok, err := vm.Arrive(engine.NewAtom("test"), []engine.Term{}, engine.Success, nil).Force(ctx)
 
-						convey.Convey("Then the result should be true and there should be no error", func() {
-							convey.So(err, convey.ShouldBeNil)
-							convey.So(ok, convey.ShouldEqual, tc.ok)
+						Convey("Then the result should be true and there should be no error", func() {
+							So(err, ShouldBeNil)
+							So(ok, ShouldEqual, tc.ok)
 						})
 					})
 				})
