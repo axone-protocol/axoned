87d75123b80ac51dd38edd4c745e76b1e25bfe74
ccamel <camel.christophe@gmail.com>
2023-01-15 00:15:28 +0100

    docs(logic): improve documentation

--- x/logic/predicate/bank.go
+++ x/logic/predicate/bank.go
@@ -10,13 +10,24 @@ import (
 	"github.com/okp4/okp4d/x/logic/util"
 )
 
-// BankBalances is a predicate which unifies the given term with the list of balances (coins) of the given account.
+// BankBalances is a predicate which unifies the given terms with the list of balances (coins) of the given account.
 //
 //	bank_balances(?Account, ?Balances)
 //
 // where:
 //   - Account represents the account address (in Bech32 format).
 //   - Coins represents the balances of the account as a list of pairs of coin denomination and amount.
+//
+// Example:
+//
+//	# Query the balances of the account.
+//	- bank_balances('okp41ffd5wx65l407yvm478cxzlgygw07h79sq0m3fm', X).
+//
+// # Query the balances of all accounts. The result is a list of pairs of account address and balances.
+// - bank_balances(X, Y).
+//
+// # Query the first balance of the given account by unifying the denomination and amount with the given terms.
+// - bank_balances('okp41ffd5wx65l407yvm478cxzlgygw07h79sq0m3fm', [-(D, A), _]).
 func BankBalances(vm *engine.VM, account, balances engine.Term, cont engine.Cont, env *engine.Env) *engine.Promise {
 	return engine.Delay(func(ctx context.Context) *engine.Promise {
 		sdkContext, err := util.UnwrapSDKContext(ctx)
---
+++ x/logic/predicate/doc.go
@@ -0,0 +1,2 @@
+// Package predicate provides a set of predicates for use with the logic module.
+package predicate
