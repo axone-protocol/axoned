af9e24df210833ed5150be0fae56012d808ce1c8
Benjamin DENEUX <b.deneux@okp4.com>
2023-01-16 11:24:48 +0100

    feat(buf): generate new proto

--- x/logic/types/query.pb.gw.go
+++ x/logic/types/query.pb.gw.go
@@ -224,9 +224,9 @@ func RegisterQueryServiceHandlerClient(ctx context.Context, mux *runtime.ServeMu
 }
 
 var (
-	pattern_QueryService_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"okp4", "okp4d", "logic", "params"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_QueryService_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"okp4", "okp4d", "logic", "params"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_QueryService_Ask_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"okp4", "okp4d", "logic", "ask"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_QueryService_Ask_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"okp4", "okp4d", "logic", "ask"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
