95fa5ff3755988017effa0363a368e4b52a49146
Benjamin DENEUX <b.deneux@okp4.com>
2023-04-06 11:56:19 +0200

    test(logic): add test for ask grpc query

---
+++ x/logic/keeper/grpc_query_ask_test.go
@@ -0,0 +1,102 @@
+package keeper_test
+
+import (
+	gocontext "context"
+	"fmt"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/cosmos/cosmos-sdk/testutil"
+	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	"github.com/golang/mock/gomock"
+	"github.com/okp4/okp4d/x/logic"
+	"github.com/okp4/okp4d/x/logic/fs"
+	"github.com/okp4/okp4d/x/logic/keeper"
+	logictestutil "github.com/okp4/okp4d/x/logic/testutil"
+	"github.com/okp4/okp4d/x/logic/types"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGRPCAsk(t *testing.T) {
+	Convey("Given a test cases", t, func() {
+		cases := []struct {
+			program        string
+			query          string
+			expectedAsnwer types.Answer
+		}{
+			{
+				program: "father(bob, alice).",
+				query:   "father(bob, X).",
+				expectedAsnwer: types.Answer{
+					Success:   true,
+					HasMore:   false,
+					Variables: []string{"X"},
+					Results: []types.Result{types.Result{Substitutions: []types.Substitution{types.Substitution{
+						Variable: "X",
+						Term: types.Term{
+							Name:      "alice",
+							Arguments: nil,
+						},
+					}}}},
+				},
+			},
+		}
+
+		for nc, tc := range cases {
+			Convey(
+				fmt.Sprintf("Given test case #%d with program: %v and query: %v",
+					nc, tc.program, tc.query), func() {
+
+					encCfg := moduletestutil.MakeTestEncodingConfig(logic.AppModuleBasic{})
+					key := storetypes.NewKVStoreKey(types.StoreKey)
+					testCtx := testutil.DefaultContextWithDB(t, key, storetypes.NewTransientStoreKey("transient_test"))
+
+					// gomock initializations
+					ctrl := gomock.NewController(t)
+					accountKeeper := logictestutil.NewMockAccountKeeper(ctrl)
+					bankKeeper := logictestutil.NewMockBankKeeper(ctrl)
+					fsProvider := logictestutil.NewMockFS(ctrl)
+
+					logicKeeper := keeper.NewKeeper(
+						encCfg.Codec,
+						key,
+						key,
+						authtypes.NewModuleAddress(govtypes.ModuleName),
+						accountKeeper,
+						bankKeeper,
+						func(ctx gocontext.Context) fs.FS {
+							return fsProvider
+						},
+					)
+					err := logicKeeper.SetParams(testCtx.Ctx, types.DefaultParams())
+
+					So(err, ShouldBeNil)
+
+					Convey("and given a query with program and query to grpc", func() {
+						queryHelper := baseapp.NewQueryServerTestHelper(testCtx.Ctx, encCfg.InterfaceRegistry)
+						types.RegisterQueryServiceServer(queryHelper, logicKeeper)
+
+						queryClient := types.NewQueryServiceClient(queryHelper)
+
+						query := types.QueryServiceAskRequest{
+							Program: tc.program,
+							Query:   tc.query,
+						}
+
+						Convey("when the grpc query ask is called", func() {
+							result, err := queryClient.Ask(gocontext.Background(), &query)
+
+							Convey("Then it should return the expected answer", func() {
+								So(err, ShouldBeNil)
+								So(result, ShouldNotBeNil)
+								So(*result.Answer, ShouldResemble, tc.expectedAsnwer)
+							})
+						})
+					})
+				})
+		}
+	})
+}
