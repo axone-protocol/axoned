dd9d132968a67d45989df73a44d1b582e87d18a5
ccamel <camel.christophe@gmail.com>
2023-04-11 19:32:21 +0200

    refactor(logic): rename fs to virtual_fs

--- x/logic/fs/virtual_fs.go
+++ x/logic/fs/virtual_fs.go
@@ -5,10 +5,6 @@ import (
 	"io/fs"
 )
 
-type FS interface {
-	fs.FS
-}
-
 // VirtualFS is the custom virtual file system used into the blockchain.
 // It will hold a list of handler that can resolve file URI and return the corresponding binary file.
 type VirtualFS struct {
@@ -16,7 +12,7 @@ type VirtualFS struct {
 	router Router
 }
 
-var _ FS = (*VirtualFS)(nil)
+var _ fs.FS = (*VirtualFS)(nil)
 
 // NewVirtualFS return a new VirtualFS object that will handle all virtual file on the interpreter.
 // File can be provided from different sources like CosmWasm cw-storage smart contract.
