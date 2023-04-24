90165010d0095e1e801ad373517f5becc3fb3570
Benjamin DENEUX <b.deneux@okp4.com>
2023-04-24 11:29:57 +0200

    test(logic): add test for params validation

--- x/logic/types/params_test.go
+++ x/logic/types/params_test.go
@@ -1,15 +1,68 @@
 package types_test
 
 import (
+	"fmt"
 	"testing"
 
+	"cosmossdk.io/math"
 	"github.com/okp4/okp4d/x/logic/types"
-	"github.com/stretchr/testify/require"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-func Test_validateParams(t *testing.T) {
-	params := types.DefaultParams()
+func TestValidateParams(t *testing.T) {
+	Convey("Given a test cases", t, func() {
+		cases := []struct {
+			name      string
+			params    types.Params
+			expectErr bool
+			err       error
+		}{
+			{
+				name:      "validate default params",
+				params:    types.DefaultParams(),
+				expectErr: false,
+				err:       nil,
+			},
+			{
+				name: "validate set params",
+				params: types.NewParams(
+					types.NewInterpreter(
+						types.WithBootstrap("bootstrap"),
+						types.WithPredicatesBlacklist([]string{"halt/1"}),
+						types.WithPredicatesWhitelist([]string{"source_file/1"}),
+					),
+					types.NewLimits(
+						types.WithMaxGas(math.NewUint(1)),
+						types.WithMaxSize(math.NewUint(2)),
+						types.WithMaxResultCount(math.NewUint(3)),
+						types.WithMaxUserOutputSize(math.NewUint(4)),
+					),
+				),
+				expectErr: false,
+				err:       nil,
+			},
+		}
 
-	// default params have no error
-	require.NoError(t, params.Validate())
+		for nc, tc := range cases {
+			Convey(
+				fmt.Sprintf("Given test case #%d: %v, with params: %v", nc, tc.name, tc.params), func() {
+
+					Convey("when validate params", func() {
+						err := tc.params.Validate()
+
+						if tc.expectErr {
+							Convey("then params validation expect error", func() {
+								So(err, ShouldNotBeNil)
+								So(err, ShouldEqual, tc.err)
+							})
+						} else {
+							Convey("then error should be nil", func() {
+								So(err, ShouldBeNil)
+							})
+						}
+
+					})
+				})
+		}
+	})
 }
