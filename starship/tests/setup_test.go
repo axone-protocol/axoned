b266250804e0f38dde22dccc88d23036dbc59c00
Anmol1696 <anmol1696@gmail.com>
2023-07-15 22:15:29 +0200

    ci(workflow): add initial codebase for okp4

---
+++ starship/tests/setup_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestE2ETestSuite(t *testing.T) {
+	suite.Run(t, new(TestSuite))
+}
+
+func (s *TestSuite) TestChainsStatus() {
+	s.T().Log("runing test for /status endpoint for each chain")
+
+	for _, chainClient := range s.chainClients {
+		status, err := chainClient.GetStatus()
+		s.Assert().NoError(err)
+
+		s.Assert().Equal(chainClient.GetChainID(), status.NodeInfo.Network)
+	}
+}
+
+func (s *TestSuite) TestChainTokenTransfer() {
+	chain1, err := s.chainClients.GetChainClient("okp4-1")
+	s.Require().NoError(err)
+
+	keyName := "test-transfer"
+	address, err := chain1.CreateRandWallet(keyName)
+	s.Require().NoError(err)
+
+	denom, err := chain1.GetChainDenom()
+	s.Require().NoError(err)
+
+	s.TransferTokens(chain1, address, 2345000, denom)
+
+	// Verify the address recived the token
+	balance, err := chain1.Client.QueryBalanceWithDenomTraces(context.Background(), sdk.MustAccAddressFromBech32(address), nil)
+	s.Require().NoError(err)
+
+	// Assert correct transfers
+	s.Assert().Len(balance, 1)
+	s.Assert().Equal(balance.Denoms(), []string{denom})
+	s.Assert().Equal(balance[0].Amount, sdk.NewInt(2345000))
+}
+
+func (s *TestSuite) TestChainIBCTransfer() {
+	chain2, err := s.chainClients.GetChainClient("gaia-1")
+	s.Require().NoError(err)
+	chain1, err := s.chainClients.GetChainClient("okp4-1")
+	s.Require().NoError(err)
+
+	keyName := "test-ibc-transfer"
+	address, err := chain1.CreateRandWallet(keyName)
+	s.Require().NoError(err)
+
+	// Tranfer atom to okp4 chain
+	s.IBCTransferTokens(chain2, chain1, address, 12345000)
+
+	// Verify the address recived the token
+	balance, err := chain1.Client.QueryBalanceWithDenomTraces(context.Background(), sdk.AccAddress(address), nil)
+	s.Require().NoError(err)
+
+	chain2Denom, err := chain2.GetChainDenom()
+	s.Require().NoError(err)
+
+	// Assert correct transfers
+	s.Assert().Len(balance, 1)
+	s.Assert().Equal(balance.Denoms(), []string{chain2Denom})
+	s.Assert().Equal(balance[0].Amount, sdk.NewInt(12345000))
+}
