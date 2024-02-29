4358459650b186effc5754da8c1a69b139e5c5a8
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2024-02-29 12:58:49 +0100

    feat(app): register v7.0.0 software upgrade

---
+++ app/upgrades.go
@@ -0,0 +1,24 @@
+package app
+
+import (
+	"fmt"
+
+	v7 "github.com/okp4/okp4d/app/upgrades/v7"
+)
+
+// RegisterUpgradeHandlers registers the chain upgrade handlers.
+func (app *App) RegisterUpgradeHandlers() {
+	app.UpgradeKeeper.SetUpgradeHandler(
+		v7.UpgradeName,
+		v7.CreateUpgradeHandler(app.ModuleManager, app.configurator),
+	)
+
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(fmt.Sprintf("failed to read upgrade info from disk %s", err))
+	}
+
+	if app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		return
+	}
+}
