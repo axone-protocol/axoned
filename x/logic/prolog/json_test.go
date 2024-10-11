cf09971ab4ca7116ddf58e85c693fce007ff1ae5
ccamel <camel.christophe@gmail.com>
2024-10-11 23:24:45 +0200

    test(logic): add test cases for json and list functions

---
+++ x/logic/prolog/json_test.go
@@ -0,0 +1,97 @@
+package prolog
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/axone-protocol/prolog/engine"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestJSONNull(t *testing.T) {
+	Convey("Given an environment", t, func() {
+		env := engine.NewEnv()
+		Convey("When calling JSONNull", func() {
+			got := JSONNull()
+			want := nullTerm
+			Convey("Then it should return the JSON null atom", func() {
+				So(got, ShouldNotBeNil)
+				So(got.Compare(want, env), ShouldEqual, 0)
+			})
+		})
+	})
+}
+
+func TestJSONBool(t *testing.T) {
+	Convey("Given a boolean value", t, func() {
+		cases := []struct {
+			input bool
+			want  engine.Term
+		}{
+			{
+				input: true,
+				want:  trueTerm,
+			},
+			{
+				input: false,
+				want:  falseTerm,
+			},
+		}
+
+		for _, tc := range cases {
+			Convey(fmt.Sprintf("When calling JSONBool(%v)", tc.input), func() {
+				got := JSONBool(tc.input)
+
+				Convey("Then the result should be as expected", func() {
+					So(got, ShouldEqual, tc.want)
+				})
+			})
+		}
+	})
+}
+
+func TestAssertJSON(t *testing.T) {
+	Convey("Given test cases", t, func() {
+		env := engine.NewEnv()
+		cases := []struct {
+			description string
+			input       engine.Term
+			wantError   error
+			wantResult  string
+		}{
+			{
+				description: "valid JSON object",
+				input:       AtomJSON.Apply(engine.NewAtom("valid")),
+				wantError:   nil,
+			},
+			{
+				description: "non-compound term",
+				input:       engine.NewAtom("notACompound"),
+				wantError:   fmt.Errorf("error(type_error(json,notACompound),root)"),
+			},
+			{
+				description: "compound term with arity > 1",
+				input:       AtomJSON.Apply(engine.NewAtom("foo"), engine.NewAtom("bar")),
+				wantError:   fmt.Errorf("error(type_error(json,json(foo,bar)),root)"),
+			},
+		}
+
+		for _, tc := range cases {
+			Convey(fmt.Sprintf("When calling AssertJSON(%s)", tc.input), func() {
+				got, err := AssertJSON(tc.input, env)
+
+				Convey("Then the result should match the expected value", func() {
+					if tc.wantError != nil {
+						So(got, ShouldBeNil)
+						So(err, ShouldBeError, tc.wantError)
+					} else {
+						So(err, ShouldBeNil)
+						So(got, ShouldNotBeNil)
+						So(got.Compare(tc.input, env), ShouldEqual, 0)
+					}
+				})
+			})
+		}
+	})
+}
---
+++ x/logic/prolog/list_test.go
@@ -0,0 +1,91 @@
+package prolog
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/axone-protocol/prolog/engine"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestForEach(t *testing.T) {
+	Convey("Given test cases", t, func(c C) {
+		env := engine.NewEnv()
+		cases := []struct {
+			description string
+			list        engine.Term
+			f           func(v engine.Term, hasNext bool) error
+			wantError   error
+		}{
+			{
+				description: "Empty list",
+				list:        engine.NewAtom("[]"),
+
+				f: func(_ engine.Term, _ bool) error {
+					t.Errorf("Function should not be called for empty list")
+					return nil
+				},
+			},
+			{
+				description: "Non-list term",
+				list:        engine.NewAtom("not_a_list"),
+
+				f: func(_ engine.Term, _ bool) error {
+					t.Errorf("Function should not be called for non-list term")
+					return nil
+				},
+				wantError: fmt.Errorf("error(type_error(list,not_a_list),root)"),
+			},
+			{
+				description: "List with elements",
+				list:        engine.List(engine.NewAtom("a"), engine.NewAtom("b"), engine.NewAtom("c")),
+
+				f: func() func(v engine.Term, hasNext bool) error {
+					i := 0
+					values := []string{"a", "b", "c"}
+					return func(v engine.Term, hasNext bool) error {
+						defer func() { i++ }()
+
+						c.So(i, ShouldBeLessThan, len(values))
+						got, err := AssertAtom(v, env)
+						c.So(err, ShouldBeNil)
+						c.So(got.String(), ShouldEqual, values[i])
+						c.So(hasNext, ShouldEqual, i < len(values)-1)
+
+						return nil
+					}
+				}(),
+				wantError: nil,
+			},
+			{
+				description: "Function returns error",
+				list:        engine.List(engine.NewAtom("a"), engine.NewAtom("b")),
+
+				f: func() func(v engine.Term, hasNext bool) error {
+					i := 0
+					return func(_ engine.Term, hasNext bool) error {
+						defer func() { i++ }()
+						c.So(i, ShouldEqual, 0)
+						c.So(hasNext, ShouldBeTrue)
+
+						return errors.New("test error")
+					}
+				}(),
+				wantError: fmt.Errorf("test error"),
+			},
+		}
+
+		for tn, tc := range cases {
+			Convey(fmt.Sprintf("When calling ForEach (case %d)", tn), func() {
+				err := ForEach(tc.list, env, tc.f)
+				if tc.wantError != nil {
+					So(err, ShouldBeError, tc.wantError)
+				} else {
+					So(err, ShouldBeNil)
+				}
+			})
+		}
+	})
+}
