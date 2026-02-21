037ac651f6d34736dc4f2cccbab26df45ff7c0a4
ccamel <camel.christophe@gmail.com>
2026-02-21 12:49:51 +0100

    refactor(logic): centralize Prolog term writing and split sys header device

--- x/logic/fs/sys/header/fs_test.go
+++ x/logic/fs/sys/header/fs_test.go
@@ -1,4 +1,4 @@
-package sys
+package header
 
 import (
 	"errors"
@@ -22,27 +22,27 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func TestSysVFSReadFile(t *testing.T) {
-	Convey("Given a sys VFS", t, func() {
-		header := coreheader.Info{
+func TestSysHeaderVFSReadFile(t *testing.T) {
+	Convey("Given a sys/header VFS", t, func() {
+		headerInfo := coreheader.Info{
 			Height:  42,
 			Hash:    []byte{1, 2, 3},
 			Time:    time.Date(2024, 4, 10, 10, 44, 27, 0, time.UTC),
 			ChainID: "axone-testchain-1",
 			AppHash: []byte{4, 5, 6},
 		}
-		vfs := NewFS(newTestContext(header))
+		vfs := NewFS(newTestContext(headerInfo))
 
 		cases := []struct {
 			path string
 			want []byte
 		}{
-			{path: "header/@", want: []byte("header{height:42,hash:[1,2,3],time:1712745867,chain_id:'axone-testchain-1',app_hash:[4,5,6]}.\n")},
-			{path: "header/height", want: []byte("42.\n")},
-			{path: "header/hash", want: []byte("[1,2,3].\n")},
-			{path: "header/time", want: []byte("1712745867.\n")},
-			{path: "header/chain_id", want: []byte("'axone-testchain-1'.\n")},
-			{path: "header/app_hash", want: []byte("[4,5,6].\n")},
+			{path: "@", want: []byte("header{app_hash:[4,5,6],chain_id:'axone-testchain-1',hash:[1,2,3],height:42,time:1712745867}.\n")},
+			{path: "height", want: []byte("42.\n")},
+			{path: "hash", want: []byte("[1,2,3].\n")},
+			{path: "time", want: []byte("1712745867.\n")},
+			{path: "chain_id", want: []byte("'axone-testchain-1'.\n")},
+			{path: "app_hash", want: []byte("[4,5,6].\n")},
 		}
 
 		for i, tc := range cases {
@@ -56,21 +56,21 @@ func TestSysVFSReadFile(t *testing.T) {
 	})
 }
 
-func TestSysVFSOpen(t *testing.T) {
-	Convey("Given a sys VFS", t, func() {
+func TestSysHeaderVFSOpen(t *testing.T) {
+	Convey("Given a sys/header VFS", t, func() {
 		headerTime := time.Date(2025, 1, 2, 3, 4, 5, 0, time.UTC)
-		header := coreheader.Info{Time: headerTime}
-		vfs := NewFS(newTestContext(header))
+		headerInfo := coreheader.Info{Time: headerTime}
+		vfs := NewFS(newTestContext(headerInfo))
 
-		Convey("when opening header/time", func() {
-			f, err := vfs.Open("header/time")
+		Convey("when opening time", func() {
+			f, err := vfs.Open("time")
 
 			So(err, ShouldBeNil)
 			defer f.Close()
 
 			info, err := f.Stat()
 			So(err, ShouldBeNil)
-			So(info.Name(), ShouldEqual, "header/time")
+			So(info.Name(), ShouldEqual, "time")
 			So(info.ModTime(), ShouldEqual, headerTime)
 
 			content, err := io.ReadAll(f)
@@ -80,19 +80,19 @@ func TestSysVFSOpen(t *testing.T) {
 	})
 }
 
-func TestSysVFSErrors(t *testing.T) {
-	Convey("Given a sys VFS", t, func() {
+func TestSysHeaderVFSErrors(t *testing.T) {
+	Convey("Given a sys/header VFS", t, func() {
 		vfs := NewFS(newTestContext(coreheader.Info{}))
 
 		Convey("when reading an unknown path", func() {
-			_, err := vfs.ReadFile("header/unknown")
+			_, err := vfs.ReadFile("unknown")
 
 			So(err, ShouldNotBeNil)
 			So(errors.Is(err, fs.ErrNotExist), ShouldBeTrue)
 		})
 
 		Convey("when reading a path escaping root", func() {
-			_, err := vfs.ReadFile("header/../height")
+			_, err := vfs.ReadFile("../height")
 
 			So(err, ShouldNotBeNil)
 			So(errors.Is(err, fs.ErrPermission), ShouldBeTrue)
@@ -100,9 +100,9 @@ func TestSysVFSErrors(t *testing.T) {
 	})
 }
 
-func newTestContext(header coreheader.Info) sdk.Context {
+func newTestContext(headerInfo coreheader.Info) sdk.Context {
 	db := dbm.NewMemDB()
 	stateStore := store.NewCommitMultiStore(db, log.NewNopLogger(), metrics.NewNoOpMetrics())
 
-	return sdk.NewContext(stateStore, cmtproto.Header{}, false, log.NewNopLogger()).WithHeaderInfo(header)
+	return sdk.NewContext(stateStore, cmtproto.Header{}, false, log.NewNopLogger()).WithHeaderInfo(headerInfo)
 }
