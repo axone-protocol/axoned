1e91505237d5e235f65e8be4ab9e6430fcf70ccd
ccamel <camel.christophe@gmail.com>
2026-02-21 15:45:27 +0100

    feat(logic): add header_info/1 predicate in chain module

--- x/logic/lib/chain.pl
+++ x/logic/lib/chain.pl
@@ -8,3 +8,11 @@ comet_info(CometInfo) :-
   open('/v1/sys/comet/@', read, Stream, [type(text)]),
   read_term(Stream, CometInfo, []),
   close(Stream).
+
+% header_info(?HeaderInfo) is det.
+%
+% Unifies HeaderInfo with the current SDK header info dict exposed by the VFS.
+header_info(HeaderInfo) :-
+  open('/v1/sys/header/@', read, Stream, [type(text)]),
+  read_term(Stream, HeaderInfo, []),
+  close(Stream).
