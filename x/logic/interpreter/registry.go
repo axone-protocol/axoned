a9ccc33b9cb289cbe664e369d8d17e8ec3e9f608
ccamel <camel.christophe@gmail.com>
2026-02-21 16:24:01 +0100

    refactor(logic)!: remove block_header/1 and migrate usage to consult + header_info/1

--- x/logic/interpreter/registry.go
+++ x/logic/interpreter/registry.go
@@ -102,7 +102,6 @@ var registry = orderedmap.New[string, any](
 		{Key: "nth1/3", Value: engine.Nth1},
 		{Key: "call_nth/2", Value: engine.CallNth},
 		{Key: "./3", Value: engine.Op3},
-		{Key: "block_header/1", Value: predicate.BlockHeader},
 		{Key: "bank_balances/2", Value: predicate.BankBalances},
 		{Key: "bank_spendable_balances/2", Value: predicate.BankSpendableBalances},
 		{Key: "bank_locked_balances/2", Value: predicate.BankLockedBalances},
